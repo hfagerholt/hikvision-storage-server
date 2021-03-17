2cd1121696d40ab8bcc073ace201b803aa73418e
Håkon Fagerholt <hakon@fagerho.lt>
2021-03-17 06:24:47 +0100

    merge pic_dl and snapshot
    Signed-off-by: "Håkon Fagerholt" <"hakon@fagerho.lt">

--- pic_dl.sh
+++ pic_dl.sh
@@ -12,3 +12,10 @@ rm $pic_tempname
 rename $pic_tempname2 $pic_tempname
 quit
 END_SCRIPT
+
+sleep 5
+# Get date format (which will be file name later)
+date=$(date '+%Y-%m-%d_%H%M')
+# Search pic folder for a pic modified within 1 minutes, if found, make a copy with datetime as name
+test=$(find $snapshotDir -name "$pic_tempname2"  -mmin -1 )
+[ ! -z $test ] && cp $snapshotDir$pic_tempname2 $snapshotDir${date}.jpg
--- snapshot.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-source "config.sh"
-sleep 20
-# Get date format (which will be file name later)
-date=$(date '+%Y-%m-%d_%H%M')
-# Search pic folder for a pic modified within 1 minutes, if found, make a copy with datetime as name
-test=$(find $snapshotDir -name "$pic_tempname2"  -mmin -1 )
-[ ! -z $test ] && cp $snapshotDir$pic_tempname2 $snapshotDir${date}.jpg
-#identify $snapshotDir$date && echo "true jpg" || rm $snapshotDir$date
