348d35b5e6e43e74fde037a58c58b6b4052ab0dd
Håkon Fagerholt <hakon@fagerho.lt>
2022-01-27 21:54:48 +0100

    Make cameraloop re-read config every restart

--- camera0.sh
+++ camera0.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-source "config.sh"
 while true
 do
+source "config.sh"
 ffmpeg -rtsp_transport tcp -stimeout 6000000 -i $streamurl -c copy -flags +global_header -f segment -strftime 1 -reset_timestamps 1 -segment_time $video_segment -segment_format mp4 ${videoDir}"%Y-%m-%d_%H_%M_%03d.mp4"
 sleep 15
 done
